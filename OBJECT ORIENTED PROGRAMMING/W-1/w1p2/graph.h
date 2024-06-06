9318f108f3754137740aa8dbbf33391b57259736
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:49:51 -0400

    Update graph.h

--- OBJECT ORIENTED PROGRAMMING/W-1/w1p2/graph.h
+++ OBJECT ORIENTED PROGRAMMING/W-1/w1p2/graph.h
@@ -1,17 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-						Workshop-1 Part-2
-Full Name  : PARAS SINGH
-Student ID#: 165114232
-Email      : psingh721@myseneca.ca
-Section    : ZEE
-Date       : 25/01/2024
-
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
-
 #ifndef SENECA_GRAPH_H
 #define SENECA_GRAPH_H
 namespace seneca {
