a8c65579cc44b8de056f32b3dc51d2bd7b0c7078
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:54:48 -0400

    Update Utils.cpp

--- OBJECT ORIENTED PROGRAMMING/W-3/w3p2/Utils.cpp
+++ OBJECT ORIENTED PROGRAMMING/W-3/w3p2/Utils.cpp
@@ -1,16 +1,3 @@
-/*///////////////////////////////////////////////////////////////////////
-                        Workshop-3 Part-2
-Full Name  : PARAS SINGH
-Student ID#: 165114232
-Email      : psingh721@myseneca.ca
-Section    : ZEE
-Date       : 07-FEB-2024
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
-
 #include <iostream>
 #include "Utils.h"
 #include <stdlib.h> // including the built-in library because size_t is a function of this library
