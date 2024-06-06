363ac52f02eef657f996ac788849a0890b9aea2c
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:49:10 -0400

    Update file.cpp

--- OBJECT ORIENTED PROGRAMMING/W-1/w1p2/file.cpp
+++ OBJECT ORIENTED PROGRAMMING/W-1/w1p2/file.cpp
@@ -1,18 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-						Workshop-1 Part-2
-Full Name  : PARAS SINGH
-Student ID#: 165114232
-Email      : psingh721@myseneca.ca
-Section    : ZEE
-Date       : 25/01/2024
-
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
-
-
 #define _CRT_SECURE_NO_WARNINGS
 #include "file.h"
 #include "StMark.h"
