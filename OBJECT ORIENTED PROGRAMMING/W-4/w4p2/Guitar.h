ad918e1665ba196e0267bb84c68cc3b2dfea94cc
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:21:15 -0400

    Update Guitar.h

--- OBJECT ORIENTED PROGRAMMING/W-4/w4p2/Guitar.h
+++ OBJECT ORIENTED PROGRAMMING/W-4/w4p2/Guitar.h
@@ -1,18 +1,4 @@
-/***********************************************************************
-// OOP244 Workshop #4 p2: tester program
-// File  main.cpp
-// Version 1.0
-// Date: Summer 2023
-// Author Michael Huang
-// Description
-//
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-//
-//
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
+
 #define _CRT_SECURE_NO_WARNINGS
 #ifndef SENECA_GUITAR_H
 #define SENECA_GUITAR_H
