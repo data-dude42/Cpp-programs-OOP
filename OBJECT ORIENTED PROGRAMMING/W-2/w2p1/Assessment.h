f3d85a2ca03978405bd0b1a843a2b614a2eb5a04
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:51:25 -0400

    Update Assessment.h

--- OBJECT ORIENTED PROGRAMMING/W-2/w2p1/Assessment.h
+++ OBJECT ORIENTED PROGRAMMING/W-2/w2p1/Assessment.h
@@ -1,15 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-                        Workshop-2 Part-1
-Full Name  : PARAS SINGH
-Student ID#: 165114232
-Email      : psingh721@myseneca.ca
-Section    : ZEE
-Date       : 28/01/2024
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
 #ifndef ASSESSMENTS_H
 #define ASSESSMENTS_H
 
