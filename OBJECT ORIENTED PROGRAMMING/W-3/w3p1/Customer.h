89f94d3d2e155e1b6c3218bf16c51aededb7ad1d
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:23:00 -0400

    Update Customer.h

--- OBJECT ORIENTED PROGRAMMING/W-3/w3p1/Customer.h
+++ OBJECT ORIENTED PROGRAMMING/W-3/w3p1/Customer.h
@@ -1,15 +1,4 @@
-/*/////////////////////////////////////////////////////////////////////////
-                        Workshop-3 Part-1
-Full Name  : PARAS SINGH
-Student ID#: 165114232
-Email      : psingh721@myseneca.ca
-Section    : ZEE
-Date       : 04-FEB-2024
 
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
 #ifndef SENECA_CUSTOMER_H
 #define SENECA_CUSTOMER_H
 #include "Car.h"
