262516934a2d95d9d90d8b2d7d21643142e790ca
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:55:45 -0400

    Update Vendor.h

--- OBJECT ORIENTED PROGRAMMING/W-3/w3p2/Vendor.h
+++ OBJECT ORIENTED PROGRAMMING/W-3/w3p2/Vendor.h
@@ -1,16 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-                        Workshop-3 Part-2
-Full Name  : PARAS SINGH
-Student ID#: 165114232
-Email      : psingh721@myseneca.ca
-Section    : ZEE
-Date       : 07-FEB-2024
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code that
-my professor provided to complete my workshops and assignments.
-/////////////////////////////////////////////////////////////////////////*/
-
 #ifndef SENECA_VENDOR_H
 #define SENECA_VENDOR_H
 #include "Icecream.h"
