b157bc336ef9075e35b95fec43e299a376806cfe
paras singh <159608516+data-dude42@users.noreply.github.com>
2024-06-06 11:56:11 -0400

    Update main.cpp

--- OBJECT ORIENTED PROGRAMMING/W-3/w3p2/main.cpp
+++ OBJECT ORIENTED PROGRAMMING/W-3/w3p2/main.cpp
@@ -1,18 +1,3 @@
-/***********************************************************************
-// OOP244 Workshop #03 p2: tester program 
-//
-// File	main.cpp
-// Version 1.0
-// Date: September 28, 2023
-// Author: Yosef Ashibani
-// Description
-//
-// Revision History
-// -----------------------------------------------------------
-// Name            Date            Reason
-//
-/////////////////////////////////////////////////////////////////
-***********************************************************************/
 #include "Vendor.h"
 using namespace seneca;
 int main() {
